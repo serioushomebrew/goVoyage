1adf7fc80d436d2b9ecac211318d330927a92ce1
Peter Schriever <peterzen72@gmail.com>
2016-12-09 23:03:18 +0100

    added redundant /api/ from api route

--- routes/api.php
+++ routes/api.php
@@ -16,8 +16,6 @@ use Illuminate\Http\Request;
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:api');
-Route::group(['prefix' => 'api'], function () {
-  Route::group(['prefix' => 'v1'], function () {
-    Route::resource('test-connection', 'TestController');
-  });
+Route::group(['prefix' => 'v1'], function () {
+  Route::resource('test-connection', 'TestController');
 });
