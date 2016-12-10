6f5b9ff363f7c393ddfd9ca6029a6e3c22ea496e
Kevin van der Burgt <kevin@zdev.com>
2016-12-10 14:10:06 +0100

    Restored some routes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>GoVoyage</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -75,15 +75,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    GoVoyage
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/serioushomebrew/goVoyage">Backend</a>
+                    <a href="https://github.com/serioushomebrew/GoVoyageMobile">Frontend App</a>
                 </div>
             </div>
         </div>
